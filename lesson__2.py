e0cee6a75c2ffa5eb82381521f5eb1d88d94a56a
zhenya-paitash <naitavv93@gmail.com>
2019-06-29 14:30:42 +0300

    resave

--- lesson__2.py
+++ lesson__2.py
@@ -2,12 +2,12 @@ a = int(input())
 b = int(input())
 
 if a > b:
-	a, b = b, a
+    a, b = b, a
 elif a == b:
-	print("data is equal")
+    print("data is equal")
 
 while b > a:
-	a+=1
-	print(a)
+    a += 1
+    print(a)
 
 print("end")
--- lesson__3.py
+++ lesson__3.py
@@ -1,17 +1,17 @@
 # массив
 a, b, c = "hello", "world", "srez"
 for x in a, b, c:
-	print(x[0], x[-1], x[::-1])
+    print(x[0], x[-1], x[::-1])
 print()
 # задаём массив
 x = "hyperlink"
-y = (x[2:4], x[5:2:-1], x[0:3],)
+y = (x[2:4], x[5:0:-1], x[0:5],)
 print(y)
 print(y[2])
 print()
 # эксперементируем
-list = [1, 3, 3, 7,]
-list2 = [3, 2, 2,]
+list = [1, 3, 3, 7, ]
+list2 = [3, 2, 2, ]
 print(list, list2)
 list2.append('end')
 print(list2)
@@ -21,5 +21,14 @@ print(list2)
 mylist = [mylist * 2 for mylist in 'hello world!']
 print(mylist)
 #
-hi = [hi*2.4 for hi in range(1,10)]
+hi = [hi * 2.4 for hi in range(1, 10)]
 print(hi)
+# тест присваивания списков
+list_a = ['список А', 1, 2, 3, 4, ]
+list_b = ['список B', 5, 6, 7, 8, ]
+print(list_a, list_b)
+list_b = list_a
+print(list_a, list_b)
+list_c = ['список C', 1, 3, 3, 7, ]
+list_a = list_c
+print(list_a, list_b, list_c)
