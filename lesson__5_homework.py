67500f29acd6cd68183fe74b6c07dd6d8ce4e27f
zhenya-paitash <naitavv93@gmail.com>
2019-07-05 21:02:54 +0300

    почти всё, осталось 13 задание

--- lesson__5_homework.py
+++ lesson__5_homework.py
@@ -1,3 +1,6 @@
+import calendar  # для 10 и 12 задания
+
+
 # Задания:
 # 1. Создадим пустую функцию которая ничего не возвращает
 def one(a):
@@ -91,12 +94,70 @@ def unclean(name):  # нечистая функция
 
 print('8: ', clean('Mia'), unclean('Jane'), m)
 # 9. Сделать функцию поиска минимума и максимума в списке
+list_nine = [5, 6, 1, 6, 0, 4, 9, 4, 1]
+
+
+def nine(n):
+    return max(n), min(n)
+
+
+print('9:  max & min =', nine(list_nine))
+
+
 # 10. Написать функцию, которая определяет, является ли год високосным.
 #     Пользователь вводит год, если он високосный, то функция возвращает True. Если нет, то False
+def cal(year):
+    return calendar.isleap(year)  # определяет, является ли год високосным(ст.модуль calendar)
+
+
+print('10:', cal(2020))
+
+
 # 11. Написать функцию season, принимающую 1 аргумент — номер месяца (от 1 до 12), и возвращающую время года,
 #     которому этот месяц принадлежит (зима, весна, лето или осень)
+def season(o):
+    if o == 12 or o == 1 or o == 2:
+        print('    winter/зима')
+    elif o == 3 or o == 4 or o == 5:
+        print('    spring/весна')
+    elif o == 6 or o == 7 or o == 8:
+        print('    summer/лето')
+    elif o == 9 or o == 10 or o == 1:
+        print('    autumn/осень')
+    else:
+        print('    Введен неверный месяц !')
+    return o
+
+
+print('11:')
+season(7)
+
+
 # 12. Написать функцию date, принимающую 3 аргумента — день, месяц и год.
 #     Вернуть True, если такая дата есть в нашем календаре, и False иначе
+def date(year, month, day):
+    year, month, day = day, month, year
+    try:
+        if calendar.weekday(year, month, day) != False:  # try - except - если выдает ошибку
+            print('    True')
+        else:
+            print('    False')
+    except:
+        print('    False')
+
+
+print('12:')
+date(21, 5, 1993)
 # 13. Список [16, -17, 2, 78.7, False, False, {‘True’: True}, 555, 12, 23, 42, ‘DD’]
 #     Функция, принимает на вход список -выбирает из него все int и float -
 #     составить из него новый список, отсортированный от наименьшего значения большему
+last = [16, -17, 2, 78.7, False, False, {‘True’: True}, 555, 12, 23, 42, ‘DD’]
+try:
+    def thirteen(*p, **r):
+        print(p)
+        print(r)
+
+except:
+    print('error')
+last.sort(key=)
+thirteen(last)
