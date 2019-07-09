2f61aeade42c107f12e5f9f7fcbfdd7cf1313355
zhenya-paitash <naitavv93@gmail.com>
2019-07-09 12:57:56 +0300

    Шестое занятие. Парсинг курса валют

---
+++ lesson__6_homework_parsing.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def get_text(url):
+    r = requests.get(url)
+    return r.json()
+
+
+def main():
+    url = "http://www.nbrb.by/API/ExRates/Rates?Periodicity=0"
+    result = get_text(url)  # равносильно записи <=>  result = requests.get(url).json() без доп.функции
+    for i in result:
+        while len(i['Cur_Name']) < 40:
+            i['Cur_Name'] += '_'
+        print(f"За {i['Date'].split('T')[0]}, цена на 1 {i['Cur_Abbreviation']}",
+              f"{i['Cur_Name']} составила {i['Cur_OfficialRate']} BYN белорусских рублей.")
+
+
+if __name__ == '__main__':
+    main()
